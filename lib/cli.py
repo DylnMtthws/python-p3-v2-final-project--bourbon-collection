5c5356d4bd9f63f224fbd4208e990e5a377a4cb2
dylanmatthews <djmatthews93@gmail.com>
2024-03-11 16:27:52 -0400

    fixed termcolor updated imports

--- lib/cli.py
+++ lib/cli.py
@@ -4,7 +4,7 @@ from helpers import (exit_program)
 from sqlalchemy import create_engine, Column, Integer, String, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker, relationship
-from termcolor import colored, cprint
+from termcolor import cprint, colored
 
 
 engine = create_engine('sqlite:///bourbon_collection.db')
